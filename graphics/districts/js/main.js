4f536cdac414f882824d2f97dca53bc9d7c9ae89
Andrew Tran <abtran@gmail.com>
2014-10-10 12:00:39 -0400

    initial districts commit

---
+++ graphics/districts/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.districts").length', function() {
+
+	var masterSelector = '.article-graphic.districts';
+	var master = $(masterSelector);
+
+}); }());
